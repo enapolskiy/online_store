0f3eb788caddc8f3aab171d4e1055a2617096efa
enapolskiy <enapolskiy83@yandex.ru>
2022-02-11 12:14:43 +0700

    route

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
 import './App.css';
 import Header from "./Header";
 import Home from "./Home";
-
-
+import {Routes, Route} from 'react-router-dom'
+import CheckOut from "./CheckOut";
 
 function App() {
   return (
     <div className="app">
         <Header />
-        <Home />
+        <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='/checkout' element={<CheckOut />} />
+        </Routes>
+
+
+
+
     </div>
   )
 }
---
+++ src/CheckOut.js
@@ -0,0 +1,20 @@
+
+
+const CheckOut = () => {
+    return (
+        <div className='checkout'>
+            <div className='checkout_left'>
+                <img src='https://images-eu.ssl-images-amazon.com/images/G/31/img21/Wireless/katariy/Category_page/Dec/D24582943_IN_WL_Category-Page_Stripe_mob_final.gif'/>
+            <div className='checkout__title'>
+                <h1>Your shopping bascet</h1>
+                </div>
+            </div>
+            <div className='checkout__right'>
+<h2>The subtotal will go here</h2>
+            </div>
+
+        </div>
+    )
+}
+
+export  default CheckOut;
