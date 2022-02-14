7b8724479bf91eb851295c458a7ed22f76a32dce
enapolskiy <enapolskiy83@yandex.ru>
2022-02-14 20:20:16 +0700

    loginPage_Front2

--- src/Login/login.jsx
+++ src/Login/login.jsx
@@ -1,7 +1,14 @@
+import React, {useState} from "react";
 import style from './login.module.css'
 import {NavLink} from "react-router-dom";
 
 const Login = () => {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('')
+    const signIn = e => {
+        e.preventDefault()
+    }
+    //3:46
     return (
         <div className={style.loginPage}>
             <NavLink to='/'>
@@ -11,10 +18,10 @@ const Login = () => {
                 <h1>Sign in</h1>
                 <form>
                     <h5>E-mail</h5>
-                    <input type='text'/>
+                    <input type='text' value={email} onChange={e => setEmail((e.target.value))}/>
                     <h5>Password</h5>
-                    <input type='password'/>
-                    <button onClick={style.signInButton}>Sign in</button>
+                    <input type='password' value={password} onChange={e => setPassword(e.target.value)}/>
+                    <button className={style.signInButton} onClick={signIn} type={"submit"}>Sign in</button>
                 </form>
                 <p>By continuing, you agree to Super Bowl's Conditions of Use and Privacy Notice.</p>
                 <button className={style.createButton}>Create your Super Bowl account</button>
